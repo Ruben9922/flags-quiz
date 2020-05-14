9d9578ac9dcb803ef92232942ddacd690dacf9be
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-05-14 16:31:33 +0100

    Add react-country-flag package and display flag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import AppBarComponent from "./AppBarComponent";
+import Container from "@material-ui/core/Container";
+import ReactCountryFlag from "react-country-flag";
 
 function App() {
-  return (
-    <AppBarComponent/>
-  );
+  return [
+    <AppBarComponent/>,
+    <Container maxWidth="md" style={{marginTop: "4em"}}>
+      <div style={{ textAlign: "center" }}>
+        <ReactCountryFlag countryCode="GB" svg style={{
+          fontSize: '10em',
+          lineHeight: '10em',
+        }}/>
+      </div>
+    </Container>
+  ];
 }
 
 export default App;
