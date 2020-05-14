1fdb727185721223ab5932509d68c9b4d615a613
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-05-14 17:41:16 +0100

    Call API to retrieve country names and alpha-2 codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,81 @@
 import React from 'react';
 import AppBarComponent from "./AppBarComponent";
 import QuizComponent from "./QuizComponent";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import {withStyles} from "@material-ui/core/styles";
+import Backdrop from "@material-ui/core/Backdrop";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return [
-    <AppBarComponent/>,
-    <QuizComponent/>
-  ];
+const styles = theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.eu/rest/v2/all?fields=name;alpha2Code")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const {classes} = this.props;
+    const {error, isLoaded, items} = this.state;
+
+    let content;
+    if (error) {
+      content =
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          Failed to load country data.
+        </Alert>;
+    } else if (!isLoaded) {
+      content =
+        <Backdrop className={classes.backdrop} open>
+          <CircularProgress color="inherit"/>
+        </Backdrop>
+    } else {
+      content = <QuizComponent countries={items}/>;
+    }
+
+    return (
+      <>
+        <AppBarComponent/>,
+        <Container maxWidth="md" style={{marginTop: "5em"}}>
+          {content}
+        </Container>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -2,17 +2,33 @@ import React from 'react';
 import Container from "@material-ui/core/Container";
 import ReactCountryFlag from "react-country-flag";
 
-function QuizComponent() {
-  return (
-    <Container maxWidth="md" style={{marginTop: "4em"}}>
-      <div style={{ textAlign: "center" }}>
-        <ReactCountryFlag countryCode="GB" svg style={{
-          fontSize: '10em',
-          lineHeight: '10em',
-        }}/>
-      </div>
-    </Container>
-  );
+class QuizComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {countries} = this.props;
+
+    return (
+      <Container maxWidth="md" style={{marginTop: "4em"}}>
+        <div style={{textAlign: "center"}}>
+          <ReactCountryFlag countryCode="GB" svg style={{
+            fontSize: '10em',
+            lineHeight: '10em',
+          }}/>
+        </div>
+        <ul>
+          {countries.map(country => (
+            <li key={country.alpha2Code}>
+              {country.name} {country.alpha2Code}
+            </li>
+          ))}
+        </ul>
+      </Container>
+    );
+  }
 }
 
 export default QuizComponent;
