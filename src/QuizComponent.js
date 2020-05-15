74010da3ce87f740dadd94b9d0633ffee4594b7d
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-05-15 18:03:56 +0100

    Add snackbars

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -6,6 +6,8 @@ import {withStyles} from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import Typography from "@material-ui/core/Typography";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
 const styles = theme => ({
   root: {
@@ -29,37 +31,51 @@ class QuizComponent extends React.Component {
       pickedCountries: pickedCountries,
       correctCountry: correctCountry,
       totalCorrect: 0,
-      totalAnswered: 0
+      totalAnswered: 0,
+      correctSnackbarOpen: false,
+      incorrectSnackbarOpen: false,
     };
   }
 
   render() {
     const {classes} = this.props;
-    const {pickedCountries, correctCountry, totalCorrect, totalAnswered} = this.state;
+    const {pickedCountries, correctCountry, totalCorrect, totalAnswered, correctSnackbarOpen, incorrectSnackbarOpen} = this.state;
 
     return (
-      <Container maxWidth="md" style={{marginTop: "4em", textAlign: "center"}}>
-        <Typography paragraph>
-          Score: {totalCorrect}/{totalAnswered}
-        </Typography>
-        <ReactCountryFlag countryCode={correctCountry.alpha2Code} svg style={{
-          fontSize: '10em',
-          lineHeight: '10em',
-        }}/>
-        <Grid container className={classes.root}>
-          {pickedCountries.map(country => (
-            <Grid item xs={6}>
-              <Card onClick={() => this.handleClick(country)} style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'column', width: "100%"}}>
-                <CardActionArea>
-                  <Typography variant="body1">
-                    {country.name}
-                  </Typography>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      <>
+        <Container maxWidth="md" style={{marginTop: "4em", textAlign: "center"}}>
+          <Typography paragraph>
+            Score: {totalCorrect}/{totalAnswered}
+          </Typography>
+          <ReactCountryFlag countryCode={correctCountry.alpha2Code} svg style={{
+            fontSize: '10em',
+            lineHeight: '10em',
+          }}/>
+          <Grid container className={classes.root}>
+            {pickedCountries.map(country => (
+              <Grid item xs={6}>
+                <Card onClick={() => this.handleClick(country)} style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'column', width: "100%"}}>
+                  <CardActionArea>
+                    <Typography variant="body1">
+                      {country.name}
+                    </Typography>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        <Snackbar key={totalAnswered} open={correctSnackbarOpen} autoHideDuration={1000} onClose={(...args) => this.handleSnackbarClose(...args)}>
+          <Alert severity="success" elevation={6} variant="filled">
+            Correct!
+          </Alert>
+        </Snackbar>
+        <Snackbar key={totalAnswered} open={incorrectSnackbarOpen} autoHideDuration={1000} onClose={(...args) => this.handleSnackbarClose(...args)}>
+          <Alert severity="error" elevation={6} variant="filled">
+            Incorrect!
+          </Alert>
+        </Snackbar>
+      </>
     );
   }
 
@@ -67,12 +83,28 @@ class QuizComponent extends React.Component {
     let pickedCountries = this.pickCountries();
     let correctCountry = this.chooseElement(pickedCountries);
 
-    this.setState(state => ({
-      pickedCountries: pickedCountries,
-      correctCountry: correctCountry,
-      totalCorrect: country === state.correctCountry ? state.totalCorrect + 1 : state.totalCorrect,
-      totalAnswered: state.totalAnswered + 1
-    }));
+    this.setState(state => {
+      let isCorrect = (country === state.correctCountry);
+      return {
+        pickedCountries: pickedCountries,
+        correctCountry: correctCountry,
+        totalCorrect: isCorrect ? state.totalCorrect + 1 : state.totalCorrect,
+        totalAnswered: state.totalAnswered + 1,
+        correctSnackbarOpen: isCorrect,
+        incorrectSnackbarOpen: !isCorrect,
+      };
+    });
+  }
+
+  handleSnackbarClose(event, reason) {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    this.setState({
+      correctSnackbarOpen: false,
+      incorrectSnackbarOpen: false,
+    });
   }
 
   pickCountries() {
