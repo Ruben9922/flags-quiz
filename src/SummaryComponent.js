86a08dcb35b1e08486d0257612ae5ace9e2c142c
Ruben9922 <Ruben9922@users.noreply.github.com>
2021-05-15 14:35:31 +0100

    Only show score if answers is non-empty

--- src/SummaryComponent.js
+++ src/SummaryComponent.js
@@ -36,65 +36,69 @@ function SummaryComponent({answers}) {
       <Typography variant="h5" component="h1" gutterBottom>
         Summary
       </Typography>
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={6} sm={5} md={4} lg={3}>
-          <Card style={{ textAlign: "center", marginBottom: "25px" }}>
-            <CardContent>
-              <Typography color="textSecondary" gutterBottom>
-                Score
-              </Typography>
-              <Typography variant="h5" component="h2">
-                {R.length(R.filter(isAnswerCorrect, answers))}/{R.length(answers)} ({(R.length(R.filter(isAnswerCorrect, answers))/R.length(answers)).toLocaleString(undefined,{ style: 'percent' })})
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
       <div className={classes.root}>
-      {R.isEmpty(answers) ? (
-        <Typography>
-          No answers to show.
-        </Typography>
-      ) : answers.map((answer, index) => (
-        <Accordion key={index}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className={classes.heading}>Question {index + 1}</Typography>
-            <Typography className={classes.secondaryHeading}>{isAnswerCorrect(answer) ? "Correct \u2705" : "Incorrect \u274C"}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Grid container direction="row" spacing={3} alignItems="center">
-              <Grid item md={2}>
-                <ReactCountryFlag
-                  countryCode={answer.correctCountry.alpha2Code}
-                  svg={!emojiSupport()}
-                  style={{
-                    fontSize: "6em",
-                  }}
-                />
+        {R.isEmpty(answers) ? (
+          <Typography>
+            No answers to show.
+          </Typography>
+        ) : (
+          <>
+            <Grid container spacing={3} justify="center">
+              <Grid item xs={6} sm={5} md={4} lg={3}>
+                <Card style={{ textAlign: "center", marginBottom: "25px" }}>
+                  <CardContent>
+                    <Typography color="textSecondary" gutterBottom>
+                      Score
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                      {R.length(R.filter(isAnswerCorrect, answers))}/{R.length(answers)} ({(R.length(R.filter(isAnswerCorrect, answers))/R.length(answers)).toLocaleString(undefined,{ style: 'percent' })})
+                    </Typography>
+                  </CardContent>
+                </Card>
               </Grid>
-              {isAnswerCorrect(answer) ? (
-                <Grid item md>
-                  <p>
-                    {answer.correctCountry.name}
-                  </p>
-                </Grid>
-              ) : (
-                <Grid item md>
-                  <p>
-                    {answer.correctCountry.name} &mdash; correct answer
-                    <br/>
-                    {answer.selectedCountry.name} &mdash; selected answer
-                  </p>
-                </Grid>
-              )}
             </Grid>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+            {answers.map((answer, index) => (
+              <Accordion key={index}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography className={classes.heading}>Question {index + 1}</Typography>
+                  <Typography className={classes.secondaryHeading}>{isAnswerCorrect(answer) ? "Correct \u2705" : "Incorrect \u274C"}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Grid container direction="row" spacing={3} alignItems="center">
+                    <Grid item md={2}>
+                      <ReactCountryFlag
+                        countryCode={answer.correctCountry.alpha2Code}
+                        svg={!emojiSupport()}
+                        style={{
+                          fontSize: "6em",
+                        }}
+                      />
+                    </Grid>
+                    {isAnswerCorrect(answer) ? (
+                      <Grid item md>
+                        <p>
+                          {answer.correctCountry.name}
+                        </p>
+                      </Grid>
+                    ) : (
+                      <Grid item md>
+                        <p>
+                          {answer.correctCountry.name} &mdash; correct answer
+                          <br/>
+                          {answer.selectedCountry.name} &mdash; selected answer
+                        </p>
+                      </Grid>
+                    )}
+                  </Grid>
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </>
+        )}
       </div>
     </>
   );
