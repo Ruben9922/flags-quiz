663297d34ac85f4ef6bb9d3a4028da9a6cf28b51
Ruben9922 <Ruben9922@users.noreply.github.com>
2021-06-20 12:21:07 +0100

    Remove unused imports to fix CI build errors

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -11,8 +11,6 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
-import {FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, Tooltip} from "@material-ui/core";
-import HelpIcon from '@material-ui/icons/Help';
 import MenuComponent from "./MenuComponent";
 
 function pickCountries(countries) {
